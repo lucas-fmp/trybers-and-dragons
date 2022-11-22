93c220c3b5b9d1b280c732118f9d4cb1b549fcc6
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-11-21 21:09:24 -0300

    Requisito 10 - criando a classe de PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,24 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _playerOne: Fighter;
+  private _playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne);
+
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
+  }
+
+  fight(): number {
+    while (
+      this._playerOne.lifePoints > 0 && this._playerTwo.lifePoints > 0
+    ) {
+      this._playerOne.attack(this._playerTwo);
+      this._playerTwo.attack(this._playerOne);
+    }
+    return super.fight();
+  }
+}
